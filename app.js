bdd77043b23fa4ba9cd95a9df2515ad2db649dff
fardisaz <amoozadeh73@yahoo.com>
2021-07-20 09:07:50 +0200

    test deployment

--- app.js
+++ app.js
@@ -46,6 +46,10 @@ mongoose
 const users = require("./routes/api/users");
 app.use("/api/users", users);
 
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'public/index.html'))
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
