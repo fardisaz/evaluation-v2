af23f33f3a4cd6c0511696a8158ffce97be19b60
fardisaz <amoozadeh73@yahoo.com>
2021-07-20 09:25:38 +0200

    feat(app):production database added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,12 @@
-module.exports = {
-  mongoURI: "mongodb://localhost:27017/evaluation-api",
-  secret: "yoursecret",
-};
+
+if(process.env.NODE_ENV==='production'){
+  module.exports = {
+    mongoURI: "mongodb+srv://user:123@cluster0.b4wjz.mongodb.net/evaluation?retryWrites=true&w=majority",
+    secret: "yoursecret",
+  };
+}else{
+  module.exports = {
+    mongoURI: "mongodb://localhost:27017/evaluation-api",
+    secret: "yoursecret",
+  };
+}
